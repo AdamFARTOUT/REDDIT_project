a196a58f3ca1592d1c82b72003af020d085a1fc6
Houssam-Ibnchakroune <h.ibnchakroune@gmail.com>
2025-10-03 18:05:07 +0100

    add src package and deleting some tests

---
+++ src/reddit_ai/db/indexes.py
@@ -0,0 +1,11 @@
+from pymongo import ASCENDING, DESCENDING
+
+def create_indexes(db):
+    posts = db.posts
+    comments = db.comments
+
+    # --- indexes (idempotent) ---
+    posts.create_index([("post_id", ASCENDING)], unique=True)
+    posts.create_index([("subreddit", ASCENDING), ("created_utc", DESCENDING)])
+    comments.create_index([("comment_id", ASCENDING)], unique=True)
+    comments.create_index([("post_id", ASCENDING), ("created_utc", DESCENDING)])
---
+++ src/reddit_ai/db/mongo.py
@@ -0,0 +1,17 @@
+from ..config import MONGO_URI, MONGO_DB
+from pymongo import MongoClient
+from functools import lru_cache
+from .indexes import create_indexes
+
+@lru_cache()
+def get_client():
+    print("Connecting to MongoDB…")
+    client = MongoClient(MONGO_URI)
+    return client
+def get_db():
+    client = get_client()
+    db = client[MONGO_DB]
+    return db   
+def ensure_indexes():
+    db = get_db()
+    create_indexes(db)
